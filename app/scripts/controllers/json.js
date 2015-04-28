3737b0761ab40293803272fd747e98554428387b
redohke <joukkue.red@gmail.com>
2015-04-28 11:58:02 +0300

    Eka commit

--- app/scripts/controllers/json.js
+++ app/scripts/controllers/json.js
@@ -10,8 +10,17 @@
 angular.module('huutoaanestysFrontApp')
   .controller('JsonCtrl', function ($scope, $http) {
     // JSON url, only json residing within the same domain is supported, for out of domain json use $http.jsonp
-    $http.get('json/kurssit.json')
+    
+	  //$scope.kurssit=('[{"tunnus":"BUS1LH001","nimi":"Yritt�jyys ja yritystoiminta","koulutusohjelma_tunnus":"HELI","laajuus":6,"ajoitus":0,"kuvaus":""},{"tunnus":"BUS1LK003","nimi":"Projektijohtaminen","koulutusohjelma_tunnus":"HELI","laajuus":3,"ajoitus":0,"kuvaus":""},{"tunnus":"ICT2TN007","nimi":"Ohjelmistokehitys","koulutusohjelma_tunnus":"TIKO","laajuus":12,"ajoitus":3,"kuvaus":"Java ohjelmistokehityst� projektiryhm�ss�"},{"tunnus":"ICT2TN009","nimi":"Tietohallinto","koulutusohjelma_tunnus":"TIKO","laajuus":3,"ajoitus":4,"kuvaus":"Tietohallinnon perusteet"},{"tunnus":"SWD4TN004","nimi":"JavaEE","koulutusohjelma_tunnus":"TIKO","laajuus":3,"ajoitus":4,"kuvaus":"Java EE tekniikoiden perusteet"},{"tunnus":"SWD4TN005","nimi":"Softalaprojekti I","koulutusohjelma_tunnus":"TIKO","laajuus":9,"ajoitus":4,"kuvaus":"Softalaprojekti I:n suoritettuaan opiskelija taitaa vaatimusm��rityksen eri vaiheissa k�ytett�vi� menetelmi�. H�n osaa m��ritt�� vaatimukset, laatia testitapaukset ja suorittaa testauksen Softalan tarjoamalle keissille."}]');
+	  
+	 /* $http.get('http://proto317.haaga-helia.fi:8080/Huutoaanestys/kurssit.json')
       .success(function(data) { //success returns a JS object
         $scope.kurssit = data; // bind the JS object to $scope
+	  */
+	  
+	  $http.get('json/kurssit.json')
+      .success(function(data) { //success returns a JS object
+        $scope.kurssit = data; // bind the JS object to $scope
+      
       });
 });
